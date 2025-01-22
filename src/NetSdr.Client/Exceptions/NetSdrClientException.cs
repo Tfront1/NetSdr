919d59fdba1020af342ae744c2f075934d794d4a
Taranko Taras <taras.taranko@intenselab.com>
2025-01-22 23:57:28 +0200

    Implement Exceptions logic

---
+++ src/NetSdr.Client/Exceptions/NetSdrClientException.cs
@@ -0,0 +1,25 @@
+﻿using NetSdr.Client.Protocol;
+
+namespace NetSdr.Client.Exceptions;
+
+public class NetSdrClientException : NetSdrException
+{
+    public ControlItem? ControlItem { get; }
+
+    public NetSdrClientException(string message) : base(message) { }
+
+    public NetSdrClientException(string message, ControlItem controlItem)
+        : base(message)
+    {
+        ControlItem = controlItem;
+    }
+
+    public NetSdrClientException(string message, Exception innerException)
+        : base(message, innerException) { }
+
+    public NetSdrClientException(string message, ControlItem controlItem, Exception innerException)
+        : base(message, innerException)
+    {
+        ControlItem = controlItem;
+    }
+}
---
+++ src/NetSdr.Client/Exceptions/NetSdrException.cs
@@ -0,0 +1,11 @@
+﻿namespace NetSdr.Client.Exceptions;
+
+public class NetSdrException : Exception
+{
+    public NetSdrException() : base() { }
+
+    public NetSdrException(string message) : base(message) { }
+
+    public NetSdrException(string message, Exception innerException)
+        : base(message, innerException) { }
+}
