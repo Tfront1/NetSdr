e3c4c29b9c4ed3a6eb2a8a4e14b5036f9bc04617
Taranko Taras <taras.taranko@intenselab.com>
2025-01-23 00:05:49 +0200

    Implement events

---
+++ src/NetSdr.Client/Events/UnsolicitedControlEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using NetSdr.Client.Protocol;
+
+namespace NetSdr.Client.Events;
+
+public class UnsolicitedControlEventArgs : EventArgs
+{
+    public ControlItem ControlItem { get; }
+    public ReadOnlyMemory<byte> Data { get; }
+
+    public UnsolicitedControlEventArgs(ControlItem controlItem, ReadOnlyMemory<byte> data)
+    {
+        ControlItem = controlItem;
+        Data = data;
+    }
+}
--- src/NetSdr.Client/IqData/IqDataProcessor.cs
+++ src/NetSdr.Client/IqData/IqDataProcessor.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
 using NetSdr.Client.Constants;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NetSdr.Client.IqData;
 
